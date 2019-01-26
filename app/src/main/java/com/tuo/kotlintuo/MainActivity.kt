401bec43884a48171ddb3be164bc99225ea91154
Tuo <839539179@qq.com>
2019-01-26 14:04:32 +0800

    测试

--- app/src/main/java/com/tuo/kotlintuo/MainActivity.kt
+++ app/src/main/java/com/tuo/kotlintuo/MainActivity.kt
@@ -3,8 +3,15 @@ package com.tuo.kotlintuo
 import android.os.Bundle
 import android.util.Log
 import com.tuo.mvp.impl.MVPActivity
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
 
-class MainActivity : MVPActivity<MainPresenter>() {
+class MainActivity : MVPActivity<MainPresenter>(),KodeinAware {
+
+
+    override val kodein: Kodein= Kodein.lazy {
+
+    }
 
     override val layoutId = R.layout.activity_main
 
